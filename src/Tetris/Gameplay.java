186df02d62d10a7a7c21b8dce7bd9cae216c140d
Charlie Harris <charlieharris@outlook.com>
2018-01-10 00:41:26 +0000

    Added additional comments

--- src/Tetris/Gameplay.java
+++ src/Tetris/Gameplay.java
@@ -9,7 +9,8 @@ import javax.swing.Timer;
 import static Tetris.Shape.rotation;
 
 class Gameplay extends JComponent implements KeyListener {
-    
+
+    /** The frame to the application */
     private Frame frame;
     /** An ArrayList of the 7 shapes to drop down */
     private ArrayList<Shape> nextShapes = new ArrayList<>();
