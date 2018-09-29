bc0261c1a4681479e4f63a5dded8f73fa46bfde5
Charlie Harris <charlieharris@outlook.com>
2018-09-29 23:34:26 +0100

    tetris v2 main

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -1,13 +1,16 @@
-package Tetris;
+package tetris;
 
 public class Main {
 
     public static void main(String[] args) {
-        Frame frame = new Frame();
-        Gameplay newGame = new Gameplay(frame);
-        frame.add(newGame);
-        frame.setVisible(true);
-        newGame.startGame();
+        Gameplay gameplay = new Gameplay();
+        Game game = new Game(
+                "Tetris",
+                Constants.GRID_WIDTH * Constants.BLOCK_SIZE + Constants.SIDE_PANEL_SIZE,
+                Constants.GRID_HEIGHT * Constants.BLOCK_SIZE + 20,
+                Constants.BACKGROUND_COLOR,
+                gameplay);
+        game.startGameplay();
     }
 
 }
