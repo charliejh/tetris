4d8da34b109241dbc28e6aa7f4683ab040555c08
Charlie Harris <charlieharris@outlook.com>
2018-10-29 23:36:18 +0000

    amended title

--- src/tetris/Main.java
+++ src/tetris/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Gameplay gameplay = new Gameplay();
         Game game = new Game(
-                "Tetrisgd",
+                "Tetris",
                 Constants.GRID_WIDTH * Constants.BLOCK_SIZE + Constants.SIDE_PANEL_SIZE,
                 Constants.GRID_HEIGHT * Constants.BLOCK_SIZE + 20,
                 Constants.BACKGROUND_COLOR,
